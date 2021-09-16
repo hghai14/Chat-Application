19ac54fe187623bafae18cad74ac488f8800d02a
Himanshi Ghai <72979568+hghai14@users.noreply.github.com>
2021-09-17 02:42:48 +0530

    Broadcast, unicast, and error handling

--- Client.java
+++ Client.java
@@ -7,17 +7,19 @@ import java.net.UnknownHostException;
 public class Client{
     static Socket forwardsSocket;
     static Socket sendMessageSocket;
+    static boolean connectionClosed = false;
 
+    // Thread for sending messages to clients
     static class ClientSendThread extends Thread{
         String userName;
         Socket socket;
     
         ClientSendThread(Socket sendMessageSocket, String clientName){
-            System.out.println("Constructing send message thread.");
             socket = sendMessageSocket;
             userName = clientName;
         }
 
+        // Message format for sending to server.
         String sendMessage(String input){
             String message = "";
             String username = "";
@@ -37,19 +39,48 @@ public class Client{
         }
     
         public void run(){
-            // PrintWriter out;
-            // BufferedReader stdIn;
+
             String message = "";
             try {
                 PrintWriter out;
                 BufferedReader stdIn ;
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String reply = "";
+
                 while (true) {
+                    // Reading message form terminal
+
                     System.out.print("> ");
                     out = new PrintWriter(socket.getOutputStream(), true);
                     stdIn = new BufferedReader(new InputStreamReader(System.in));
                     message = stdIn.readLine();
                     message = sendMessage(message);
                     out.println(message);
+
+                    // Waiting for acknowledgement
+                    while(true) {
+                        if(connectionClosed){
+                            socket.close();
+                            return;
+                        }
+                        reply = in.readLine();
+                        if(reply != null && reply.length() >= 4){
+                            if(reply.substring(0, 4).equals("SEND")){
+                                System.out.println(reply);
+                                break;
+                            }
+                            else if(reply.substring(0, 9).equals("ERROR 103")){
+                                System.out.println("Reconnect to send messages. Connection closed.");
+                                connectionClosed = true;
+                                forwardsSocket.close();
+                                return;
+                            }
+                            else{
+                                System.out.println(reply);
+                                break;
+                            }
+                        }
+                    }
                 }
 
             } catch (IOException e) {
@@ -62,12 +93,12 @@ public class Client{
         
     }
 
+    // Thread to receive messages from other clients
     static class ClientReceiveThread extends Thread{
         String userName;
         Socket socket;
     
         ClientReceiveThread(Socket forwardsSocket, String clientName){
-            System.out.println("Constructing send message thread.");
             socket =  forwardsSocket;
             userName = clientName;
         }
@@ -75,15 +106,37 @@ public class Client{
         public void run(){
                 try {
                     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
                     String line;
+                    
+                    // Waiting for forwards from server 
                     while(true) {
+                        if(connectionClosed){
+                            socket.close();
+                            return;
+                        }
                         line = in.readLine();
                         if(line != null && line.length() != 0 && !line.equals(" ")){
-                            System.out.println("< " + line);
+                            String username = line.substring(8);
+                            line = in.readLine();
+
+                            int len = Integer.valueOf(line.substring(16));
+
+                            line = in.readLine();
+                            line = in.readLine();
+
+                            // Sending acknowledgement to server
+                            if(line.length() != len){
+                                out.println("ERROR 103 Header Incomplete\n");
+                            }
+                            else{
+                                out.println("RECEIVED " + username + "\n");
+                                System.out.println("From " + username + ":" + line);
+                                System.out.print("> ");
+                            }
                         }
                     }
                 } catch (Exception e) {
-                    // TODO Auto-generated catch block
                     e.printStackTrace();
                 };
         }
@@ -104,54 +157,71 @@ public class Client{
         String username = args[0];
         InetAddress server;
 
+        // Connecting to server to receive and send messages.
         try {
             server = InetAddress.getByName(args[1]);
             try {
+                // Register to reveive messages from other clients.
                 forwardsSocket = new Socket(server, 8080);
 
                 PrintWriter out = new PrintWriter(forwardsSocket.getOutputStream(), true);
                 BufferedReader in = new BufferedReader(new InputStreamReader(forwardsSocket.getInputStream()));
                 BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
 
+                // Sending request to server
                 out.println("REGISTER TORECV " + username + "\n \n");
 
+                // Waiting for acknowledgement
+                String line;
                 while(true){
-                    String line = in.readLine();
-                    if(line == null){
+                    line = in.readLine();
+                    if(line == null || line.length() <= 9){
                         continue;
                     }
                     else if(line.substring(0, 9).equals("ERROR 100")){
+                        // Username malformed.
                         System.out.println(line);
                         System.out.println("Enter new username:");
+
                         stdIn = new BufferedReader(new InputStreamReader(System.in));
                         username = stdIn.readLine();
+                        
+                        // Again requesting to rgister with new username
                         out.println("REGISTER TORECV " + username + "\n \n");
                     }
                     else if(line.substring(0, 17).equals("REGISTERED TORECV")){
+                        // Succesfully registered.
                         System.out.println("User Registered to Receive");
                         break;
                     }
                 }
 
+                // Register to send messages to other clients
                 sendMessageSocket = new Socket(server, 8080);
                 out = new PrintWriter(sendMessageSocket.getOutputStream(), true);
                 in = new BufferedReader(new InputStreamReader(sendMessageSocket.getInputStream()));
                 
                 out.println("REGISTER TOSEND " + username + "\n \n");
 
+                // waiting for acknowledgement
                 while(true){
-                    String line = in.readLine();
+                    line = in.readLine();
                     if(line == null){
                         continue;
                     }
                     else if(line.substring(0, 9).equals("ERROR 100")){
+                        // Already used username
                         System.out.println(line);
                         System.out.println("Enter new username:");
+
                         stdIn = new BufferedReader(new InputStreamReader(System.in));
                         username = stdIn.readLine();
+
+                        // Requesting again with new username
                         out.println("REGISTER TOSEND " + username + "\n \n");
                     }
                     else if(line.substring(0, 17).equals("REGISTERED TOSEND")){
+                        // Succesfully registered
                         System.out.println("User Registered to Send");
                         break;
                     }
@@ -160,7 +230,7 @@ public class Client{
             } catch (IOException e) {
                 System.out.println(e);;
             }
-
+            // Openeing threads for sending and receiving messages
             ClientReceiveThread receiveThread = new Client.ClientReceiveThread(forwardsSocket, username);
             receiveThread.start();
 
--- Server.java
+++ Server.java
@@ -4,6 +4,7 @@ import java.net.Socket;
 import java.util.*;
 
 
+
 public class Server{
     ServerSocket serverSoc;
     ArrayList<Socket> active_sockets = new ArrayList<>(); 
@@ -19,74 +20,224 @@ public class Server{
         public void run(){
             
             BufferedReader in;
+            PrintWriter out;
+
             try {
+
                 in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                out = new PrintWriter(socket.getOutputStream(), true);
+                String message;
+
                 while(true){
-                    String message = in.readLine();
+                    message = in.readLine();
+
                     if(message != null) {
-                        System.out.println(message);
+
                         if(message.length() == 0 || message.equals(" ")) continue;
 
-                        if(message.substring(0, 15).equals("REGISTER TORECV")){
+                        // Register client to receive messages
+                        if(message.length() >= 15 && message.substring(0, 15).equals("REGISTER TORECV")){
                             if(validUserName(message.substring(16)) && registered_users.get(message.substring(16)) == null){
+
+                                // Valid username, adding socket ot hastable
                                 registered_users.put(message.substring(16), socket);
+
+                                // Reply to client 
                                 out.println("REGISTERED TORECV " + message.substring(16) + "\n \n");
+
                                 return;
                             }
-                            else if(registered_users.get(message.substring(16)) != null){
-                                out.println("ERROR 101");
-                            }
-                            else{
-                                out.println("ERROR 100");
+                            else if(!validUserName(message.substring(0, 16))){
+                                // Reply to client on unvalid username
+                                out.println("ERROR 100 Malformed username\n \n");
                             }
                             
                         }
+                        // Register client to send messages
                         else if(message.substring(0, 15).equals("REGISTER TOSEND")){
+                            // If registered to receive messages then
                             if(registered_users.get(message.substring(16)) != null){
+
+                                // Registering client to send messages
                                 clientName = message.substring(16);
+
                                 out.println("REGISTERED TOSEND " + message.substring(16) + "\n \n");
                             }
                             else{
-                                out.println("ERROR 101");
+                                out.println("ERROR 100 Malformed username");
                                 continue;
                             }
+
+                            // Thread open to receive messages from client to forward it to other users.
                             String line;
                             while(true){ 
                                 line = in.readLine();  
                                 if(line != null) {
                                     if(line.length()>4 && line.substring(0, 4).equals("SEND")){
+                                        // Breadcast Messages
                                         if(line.substring(5).equals("ALL")){
-                                            Enumeration<Socket> receipents = registered_users.elements();
-                                            
+
+                                            line = in.readLine();
+                                            int len = Integer.valueOf(line.substring(16));
+
+                                            line = in.readLine();
+                                            line = in.readLine();
+                                            String content = line;
+
+                                            // Header Packet Error, closing connections with client
+                                            if(content.length() != len){
+                                                out.println("ERROR 103 Header Incomplete\n \n");
+                                                // Closing both sockets of current client
+                                                Socket forwardsSocket = registered_users.get(clientName);
+                                                forwardsSocket.close();
+                                                registered_users.remove(clientName);
+                                                socket.close();
+                                                return;
+                                            }
+
+                                            // Broadcasting message and checking if success
+                                            boolean result = broadcastMessage(content);
+
+                                            if(result) {
+                                                out.println("SEND ALL\n");
+                                            }
+                                            else{
+                                                out.println("ERROR 102 Unable to send\n \n");
+                                            }
+
                                             continue;
                                         }
-                                        Socket receiverClientSocket = registered_users.get(line.substring(5));
-                                        line = in.readLine();
 
+                                        // Unicast Messages
+                                        String receiver = line.substring(5);
+
+                                        line = in.readLine();
                                         int len = Integer.valueOf(line.substring(16));
 
                                         line = in.readLine();
                                         line = in.readLine();
-                                        
-                                        // content length mismatch error
+                                        String content = line;
 
-                                        String forward_message = "FORWARD " + clientName + "\n" + "Content-Length: " + Integer.toString(len) + "\n" +"\n" + line;
-                                        PrintWriter receiverOut = new  PrintWriter(receiverClientSocket.getOutputStream(), true);
-                                        receiverOut.println(forward_message);
+                                        // Header Packet Error, closing connections with client
+                                        if(content.length() != len){
+                                            out.println("ERROR 103 Header Incomplete\n \n");
+                                            // Closing both sockets of current client
+                                            Socket forwardsSocket = registered_users.get(clientName);
+                                            forwardsSocket.close();
+                                            registered_users.remove(clientName);
+                                            socket.close();
+                                            return;
+                                        }
+
+                                        boolean result = unicastMessage(line, receiver);
+
+                                        if(result) {
+                                            out.println("SEND " + receiver + "\n");
+                                        }
+                                        else{
+                                            out.println("ERROR 102\n");
+                                        }
                                     }
                                 }
                             }
+                        }
+                        else{
+                            out.println("ERROR 101 No user registered\n \n");
                         }
 
                     } 
                 }
             } catch (IOException e1) {
-                // TODO Auto-generated catch block
                 e1.printStackTrace();
             }
         }
+
+        private boolean broadcastMessage(String content) {
+            int len = content.length();
+            String forwardMessage = "FORWARD " + clientName + "\n" + "Content-Length: " + Integer.toString(len) + "\n" +"\n" + content;
+            PrintWriter receiverOut;
+            boolean success = true;
+            for (Map.Entry<String, Socket> e : registered_users.entrySet()){
+                if(!e.getKey().equals(clientName)){
+                    Socket currClient = e.getValue();
+                    try {
+                        receiverOut = new PrintWriter(currClient.getOutputStream(), true);
+                        receiverOut.println(forwardMessage);
+                        BufferedReader in = new BufferedReader(new InputStreamReader(currClient.getInputStream()));
+                        String reply = in.readLine();
+                        while(true) {
+                            if(reply == null || reply.length() < 4 || reply.equals(" ")){
+                                continue;
+                            }
+                            if(reply.substring(0, 8).equals("RECEIVED")) {
+                                break;
+                            }
+                            else if(reply.substring(0, 9).equals("ERROR 103")){
+                                success = false;
+                                break;
+                            }
+
+                        }
+                        if(!success){
+                            break;
+                        }
+
+                    } catch (IOException e1) {
+                        // TODO Auto-generated catch block
+                        e1.printStackTrace();
+                    }
+                }
+            }
+            if(success) {
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        private boolean unicastMessage(String content, String recipient){
+            Socket receiverClientSocket = registered_users.get(recipient);
+            int len = content.length();
+            String forward_message = "FORWARD " + clientName + "\n" + "Content-Length: " + Integer.toString(len) + "\n" +"\n" + content;
+            PrintWriter receiverOut;
+            try {
+                receiverOut = new  PrintWriter(receiverClientSocket.getOutputStream(), true);
+                receiverOut.println(forward_message);
+                boolean success = true;
+                while (true){
+                    BufferedReader in = new BufferedReader(new InputStreamReader(receiverClientSocket.getInputStream()));
+                    String reply = in.readLine();
+                    while(true) {
+                        if(reply == null || reply.length() < 4 || reply.equals(" ")){
+                            continue;
+                        }
+                        if(reply.substring(0, 8).equals("RECEIVED")) {
+                            break;
+                        }
+                        else if(reply.substring(0, 9).equals("ERROR 103")){
+                            success = false;
+                            break;
+                        }
+                    }
+
+                    if(success) {
+                        return true;
+                    }
+                    else{
+                        return false;
+                    }
+                }
+
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            return true;
+        }
     }
+
+    
 
     public Server(){
         try {
